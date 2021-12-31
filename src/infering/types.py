90013d6d2d75dd31febc1c6a1a87faf38f6f26ba
Daniel Frédéric <daniel.frederic@epita.fr>
2021-12-31 01:45:04 +0100

    New testsuite, some progresses on infering

--- src/infering/types.py
+++ src/infering/types.py
@@ -46,7 +46,7 @@ def get_type_binop(node, vardict={}):
         right = get_type(node.right, vardict)
         if left is None or right is None:
             return None
-        if node.op.__class__ == ast.Add or node.op.__class__ == ast.Sub or node.op.__class__ == ast.Mult or node.op.__class__ == ast.Div or node.op.__class__ == ast.Pow or node.op.__class__ == ast.Mod or node.op.__class__ == ast.FloorDiv or node.op.__class__ == ast.BitOr or node.op.__class__ == ast.BitAnd or node.op.__class__ == ast.BitXor:
+        if node.op.__class__ == ast.Add or node.op.__class__ == ast.Sub or node.op.__class__ == ast.Mult or node.op.__class__ == ast.Div or node.op.__class__ == ast.Mod or node.op.__class__ == ast.FloorDiv or node.op.__class__ == ast.BitOr or node.op.__class__ == ast.BitAnd or node.op.__class__ == ast.BitXor:
             if left == 'int' and right == 'int':
                 return 'int'
             elif left == 'float' and right == 'float':
@@ -82,6 +82,33 @@ def get_type_binop(node, vardict={}):
                 return 'u32'
             elif left == 'u64' and right == 'u64':
                 return 'u64'
+            else:
+                return None
+        elif node.op.__class__ == ast.Eq or node.op.__class__ == ast.NotEq or node.op.__class__ == ast.Lt or node.op.__class__ == ast.LtE or node.op.__class__ == ast.Gt or node.op.__class__ == ast.GtE:
+            if left == 'int' and right == 'int':
+                return 'bool'
+            elif left == 'float' and right == 'float':
+                return 'bool'
+            elif left == 'str' and right == 'str':
+                return 'bool'
+            elif left == 'bool' and right == 'bool':
+                return 'bool'
+            elif left == 'NoneType' and right == 'NoneType':
+                return 'bool'
+            elif left == 'list' and right == 'list':
+                return 'bool'
+            elif left == 'u16' and right == 'u16':
+                return 'bool'
+            elif left == 'u32' and right == 'u32':
+                return 'bool'
+            elif left == 'u64' and right == 'u64':
+                return 'bool'
+            elif left == 'i16' and right == 'i16':
+                return 'bool'
+            elif left == 'i32' and right == 'i32':
+                return 'bool'
+            elif left == 'i64' and right == 'i64':
+                return 'bool'
             else:
                 return None
         return None
@@ -200,7 +227,7 @@ def get_type_call(node, vardict={}):
             return None
         l = type_of_call.split(' -> ')
         type_in_vardict = vardict[node.func.id].split(' -> ') if str(node.func.id) in vardict else None
-        print(vardict)
+        # print(vardict)
         #if type_in_vardict is None:
         #    return type_of_call
         if len(l) != len(type_in_vardict):
@@ -220,7 +247,7 @@ def get_type_call(node, vardict={}):
                     #     type_in_vardict[j] = new if type_in_vardict[j] == change else type_in_vardict[j]
                 else:
                     return None
-        print(l[-1])
+        # print(l[-1])
         return l[-1]
         # return ' -> '.join(l)
         # return type_of_call
@@ -249,6 +276,8 @@ def get_type_tuple(node, vardict={}):
 #returns None if the type cannot be deduced
 def get_type(node, vardict={}):
     if isinstance(node, ast.Constant):
+        if str(node.value) in BUILTIN_TOKENS:
+            return BUILTIN_TOKENS[str(node.value)]
         return get_token_type(type(node.value))
     elif isinstance(node, ast.Name):
         return vardict[node.id] if node.id in vardict else None
@@ -262,6 +291,54 @@ def get_type(node, vardict={}):
         return get_type_list(node, vardict)
     elif isinstance(node, ast.Tuple):
         return get_type_tuple(node, vardict)
+
+class wrapper:
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return str(self.value)
+
+RED = "\033[31m"
+GREEN = "\033[32m"
+DEFAULT = "\033[39m"
+
+def check_test(test_case :str, expected :str, env : dict, failed_number : wrapper, success_number : wrapper, failed_list):
+    try:
+        actual = get_type(ast.parse(test_case).body[0].value, env)
+        if str(actual) == expected:
+            success_number.value += 1
+        else:
+            failed_number.value += 1
+            failed_list.append(f'{RED}[KO]{DEFAULT} {test_case} w. {env} -> got {actual} != {expected}')
+    except Exception as e:
+        failed_number.value += 1
+        failed_list.append(f'{RED}[KO]{DEFAULT} {test_case} w. {env} -> {e}\n{__import__("traceback").format_exc()}')
+    return failed_number, success_number, failed_list
+
+def erase():
+    print('\033[F\033[K', end='')
+
+def test(cases :list, envs :list, expected :list):
+    percentage = 0.
+    failed_number = wrapper(0)
+    success_number = wrapper(0)
+    failed_list = []
+    total = len(cases) * len(envs)
+    len_expected = 0
+    for i in expected:
+        len_expected += len(i)
+    if len_expected != total:
+        print(f'{RED}[KO]{DEFAULT} The number of expected results is different from the number of tests : {len_expected} results vs. {total} tests')
+        return
+    print("")
+    for i in range(len(cases)):
+        for j in range(len(envs)):
+            failed_number, success_number, failed_list = check_test(cases[i], expected[i][j], envs[j], failed_number, success_number, failed_list)
+            percentage = (success_number.value / (success_number.value + failed_number.value)) * 100
+        erase()
+        print(f'[Success. {GREEN}{success_number.value}{DEFAULT}/{total}] [Failed. {RED if failed_number.value != 0 else ""}{failed_number.value}{DEFAULT if failed_number.value != 0 else ""}/{total}]')
+    for i in failed_list:
+        print(i)
 
 if __name__=='__main__':
     test_cases = [
@@ -283,6 +360,7 @@ if __name__=='__main__':
         'inexistant',
         'a + inexistant',
         'a + 1',
+        'a + 1.',
         'a + None',
         'a < 2',
         'a < b',
@@ -297,11 +375,53 @@ if __name__=='__main__':
         '(1, 2, 3, 4, 5.0)',
     ]
 
-    for test in test_cases:
-        print('New test : ' + test)
-        # print(get_type(ast.parse(test).body[0].value))
-        # print(get_type(ast.parse(test).body[0].value, {'a': 'int'}))
-        # print(get_type(ast.parse(test).body[0].value, {'a': 'float'}))
-        print(get_type(ast.parse(test).body[0].value, {'a': 'float', 'pika': 'int -> int -> float -> float'}))
-        # print(get_type(ast.parse(test).body[0].value, {'a': 'float'}))
+    envs = [
+        {'a': 'int'},
+        {'a': 'float'},
+        {'a': 'float', 'pika': 'int -> int -> float -> float'},
+    ]
+
+    expected = [
+        ['int', 'int', 'int'],
+        ['int', 'int', 'int'],
+        ['int', 'int', 'int'],
+        ['int', 'int', 'int'],
+        ['None', 'None', 'None'],
+        ['int', 'int', 'int'],
+        ['float', 'float', 'float'],
+        ['str', 'str', 'str'],
+        ['bool', 'bool', 'bool'],
+        ['bool', 'bool', 'bool'],
+        ['NoneType', 'NoneType', 'NoneType'],
+        ['int', 'int', 'int'],
+        ['int', 'int', 'int'],
+        ['int', 'int', 'int'],
+        ['int', 'float', 'float'],
+        ['None', 'None', 'None'],
+        ['None', 'None', 'None'],
+        ['int', 'None', 'None'],
+        ['None', 'float', 'float'],
+        ['None', 'None', 'None'],
+        ['bool', 'None', 'None'],
+        ['None', 'None', 'None'],
+        ['None', 'None', 'None'],
+        ['int -> int -> int -> float', 'float', 'float'],
+        ['int -> int -> int -> float', 'float', 'float'],
+        ['None', 'None', 'None'],
+        ['NoneType', 'NoneType', 'NoneType'],
+        ['list of int', 'list of int', 'list of int'],
+        ['None', 'None', 'None'],
+        ['(int, int, int)', '(int, int, int)', '(int, int, int)'],
+        ['(int, int, int, int, float)', '(int, int, int, int, float)', '(int, int, int, int, float)']
+    ]
+
+    test(test_cases, envs, expected)
+
+    #for test in test_cases:
+    #    print('New test : ' + test)
+    #    # print(get_type(ast.parse(test).body[0].value))
+    #    # print(get_type(ast.parse(test).body[0].value, {'a': 'int'}))
+    #    # print(get_type(ast.parse(test).body[0].value, {'a': 'float'}))
+    #    print(get_type(ast.parse(test).body[0].value, {'a': 'float', 'pika': 'int -> int -> float -> float'}))
+    #    # print(get_type(ast.parse(test).body[0].value, {'a': 'float'}))
 
