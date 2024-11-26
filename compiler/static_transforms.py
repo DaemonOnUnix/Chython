41cb6bf09d792721c15d08e22cfc1580f4b47bfe
DaemonOnUnix <daniel.frederic@lse.re>
2024-11-26 22:26:21 +0100

    parser: Simplify -Cst and +Cst

--- compiler/static_transforms.py
+++ compiler/static_transforms.py
@@ -1,27 +1,40 @@
 import ast
 
 # Modelize before parsing AST trasnformation
+
 # For instance Attribute(value=Name(id='self', ctx=Load()), attr='truc', ctx=Store())
 # Becoming Name(id='self->truc', ctx=Store())
-
 class StaticTransform(ast.NodeTransformer):
     def visit_Attribute(self, node):
-        # Visit the child nodes first
         self.generic_visit(node)
         
-        # Check if the attribute is of the desired pattern
         if isinstance(node.value, ast.Name) and isinstance(node.ctx, ast.Store):
-            # Transform Attribute to Name with the desired format
             new_node = ast.Name(
                 id=f"{node.value.id}->{node.attr}",
                 ctx=node.ctx
             )
             return ast.copy_location(new_node, node)
+        return node
+
+# Transforme UnaryOp (- Constant to Constant)
+class StaticTransformUnaryOp(ast.NodeTransformer):
+    def visit_UnaryOp(self, node):
+        self.generic_visit(node)
         
-        return node  # If no transformation, return the original node
+        if isinstance(node.op, ast.USub) and isinstance(node.operand, ast.Constant):
+            new_node = ast.Constant(
+                value=-node.operand.value
+            )
+            return ast.copy_location(new_node, node)
+        if isinstance(node.op, ast.UAdd) and isinstance(node.operand, ast.Constant):
+            new_node = ast.Constant(
+                value = node.operand.value
+            )
+            return ast.copy_location(new_node, node)
 
 TRANSFORM_LIST = [
-    StaticTransform
+    StaticTransform,
+    StaticTransformUnaryOp
 ]
 
 def apply_transfos(astNode):
